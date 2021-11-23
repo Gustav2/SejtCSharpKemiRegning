1ee77c915d0e632060e06b96b44eeda3180889bb
Gustav2 <gustavnybro@protonmail.com>
2021-11-23 19:55:32 +0100

    IO started

--- SejtKemiRegning/SejtKemiRegning/InputOutput.cs
+++ SejtKemiRegning/SejtKemiRegning/InputOutput.cs
@@ -1,7 +1,12 @@
-﻿namespace SejtKemiRegning
+﻿using System.Collections.Generic;
+
+namespace SejtKemiRegning
 {
     public class InputOutput
     {
-        
+        public static Dictionary<string, double> CSVToDict(string path)
+        {
+            return new Dictionary<string, double>();
+        }
     }
 }
