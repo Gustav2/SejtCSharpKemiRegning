b37d0e884117da4cd23686a1f2402ed9c8432a54
Gustav2 <gustavnybro@protonmail.com>
2021-11-30 13:18:39 +0100

    added input/output code that parses csv file

--- SejtKemiRegning/SejtKemiRegning/InputOutput.cs
+++ SejtKemiRegning/SejtKemiRegning/InputOutput.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace SejtKemiRegning
 {
@@ -6,7 +8,31 @@ namespace SejtKemiRegning
     {
         public static Dictionary<string, double> CSVToDict(string path)
         {
-            return new Dictionary<string, double>();
+            Dictionary<string, double> dict = new Dictionary<string, double>();
+
+            
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    if (values.Length == 4)
+                    {
+                        try
+                        {
+                            //uses index 1 for element and 3 for molmass
+                            dict.Add(values[1], double.Parse(values[3]));
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+                }
+            }
+
+            return dict;
         }
     }
 }
